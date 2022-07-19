faa79674639a86a152b603167a870f076ef1d21a
ChandanaSNambiar <chandana.s.nambiar@gmail.com>
2022-07-19 13:03:56 +0530

    Initial commit

---
+++ src/main/java/com/todos/services/TodosService.java
@@ -0,0 +1,16 @@
+package com.todos.services;
+
+import java.util.List;
+
+import com.todos.models.Todo;
+
+public interface TodosService {
+
+	List<Todo> getAllTodos();
+	List<Todo> getTodosByUser(String user);
+	Todo getTodoById(int id);
+	Todo addToDo(Todo todo);
+	Todo updateTodo(String name,int id,Todo todo);
+	boolean deleteTodo(int id);
+}
+
